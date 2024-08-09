9951331d412de256d2267be446801c613e578ff2
Max02well <onyangogogo2002@gmail.com>
2024-08-09 17:59:03 +0300

    Added the view for pdf and autobio

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import SectionTitle from '../../components/SectionTitle'
 import logo from '../../assets/IMG_9313.jpg'
-import luo from '../../assets/Maxwell_GogoCV-LUO.pdf'
-import swahili from '../../assets/Maxwell_GogoCV-swahili.pdf'
-import english from '../../assets/Maxwell_GogoCV-English.pdf'
+
 
 function About() {
-    const skills =[
+
+    const skills = [
         'React Native',
         'React',
         'Node.js',
@@ -15,44 +14,34 @@ function About() {
         'C#',
         'Figma',
     ]
-  return (
-    <div>
-        <SectionTitle title={'About Me'} />
-        <div className='flex w-full gap-10 items-center sm:flex-col'>
-            <div className='w-1/2 h-[50vh]'>
-                <img src={logo} alt='about' className='w-full h-full object-contain sm:w-full rounded-3xl' />
-            </div>
-            <div className='w-1/2 flex flex-col gap-5 sm:w-full'>
-                <h1 className='text-white text-4xl font-semibold'>Who am I?</h1>
-                <p className='text-white text-xl'>Hello,My name is Maxwell Gogo.I am a fourth year student at Dedan Kimathi University of Technology,Nyeri-Kenya.I am pursuing Bachelor of Science in Information Technology and currently am in my last semester.</p>
-                <p className='text-secondary-100 text-xl'>I am passionate about mobile development with React Native and as well UI/UX design which puts the user of an app as the center of every software development that ensures they have an interesting experience with the app.</p>
-            </div>
-        </div>
-        <div className='py-5 mt-5' >
-            <h1 className='text-green-300 text-xl'>These are the technologies I've been working with recently:</h1>
-            <div className='flex flex-wrap gap-10 mt-5'>
-            {skills.map((skill,index)=>(
-                <div key={index} className='flex items-center gap-5 border border-green-200 py-3 px-7 rounded'>
-                    <h1 className='text-white text-lg'>{skill}</h1>
-                    
+
+    
+
+    return (
+        <div>
+            <SectionTitle title={'About Me'} />
+            <div className='flex w-full gap-10 items-center sm:flex-col'>
+                <div className='w-1/2 h-[50vh] sm:w-full'>
+                    <img src={logo} alt='about' className='w-full h-full object-contain rounded-3xl' loading="lazy" />
                 </div>
-            ))}  
+                <div className='w-1/2 flex flex-col gap-5 sm:w-full'>
+                    <h1 className='text-white text-4xl font-semibold'>Who am I?</h1>
+                    <p className='text-white text-xl'>Hello, My name is Maxwell Gogo. I am a fourth year student at Dedan Kimathi University of Technology, Nyeri-Kenya. I am pursuing Bachelor of Science in Information Technology and currently am in my last semester.</p>
+                    <p className='text-secondary-100 text-xl'>I am passionate about mobile development with React Native and as well UI/UX design which puts the user of an app as the center of every software development that ensures they have an interesting experience with the app.</p>
+                </div>
             </div>
-            <div className='flex gap-7 flex-row mt-12 items-center justify-center'>
-                <a href={english} download="Resume">
-                    <button className='text-white bg-blue-500 px-4 py-2 rounded'>Download CV(English)</button>
-                </a>
-                <a href={swahili} download="Resume">
-                    <button className='text-white bg-blue-500 px-4 py-2 rounded'>Download CV(Kiswahili)</button>
-                </a>
-                <a href={luo} download="Resume">
-                    <button className='text-white bg-blue-500 px-4 py-2 rounded'>Download CV(Luo)</button>
-                </a>
+            <div className='py-5 mt-5'>
+                <h1 className='text-green-300 text-xl'>These are the technologies I've been working with recently:</h1>
+                <div className='flex flex-wrap gap-10 mt-5'>
+                    {skills.map((skill, index) => (
+                        <div key={index} className='flex items-center gap-5 border border-green-200 py-3 px-7 rounded'>
+                            <h1 className='text-white text-lg'>{skill}</h1>
+                        </div>
+                    ))}
+                </div>
+              </div>
             </div>
-        </div>
-      
-    </div>
-  )
+    )
 }
 
 export default About
--- src/pages/Home/Capstone.js
+++ src/pages/Home/Capstone.js
@@ -1,35 +1,104 @@
-import React from 'react'
-import SectionTitle from '../../components/SectionTitle'
-import { capstone } from '../../resources/capstone'
-
+import React, { useState } from 'react';
+import SectionTitle from '../../components/SectionTitle';
+import { capstone } from '../../resources/capstone';
+import luo from '../../assets/Maxwell_GogoCV-LUO.pdf';
+import swahili from '../../assets/Maxwell_GogoCV-swahili.pdf';
+import english from '../../assets/Maxwell_GogoCV-English.pdf';
+import autoBio from '../../assets/AutoBio.pdf';
 
 function Capstone() {
-    const [selectionItemIndex, setSelectionItemIndex] = React.useState(0)
-    return (
-      <div>
-          <SectionTitle title={'Capstone Project 4.2'} />
-          <div className='flex py-10 gap-20 sm:flex-col'>
-              <div className='sm:flex-row sm:overflow-scroll sm:w-full flex flex-col gap-7 w-1/3 border-l-2 border-teal-200'>
-              {capstone.map((capstone,index)=>(
-                  <div onClick={()=>{
-                      setSelectionItemIndex(index)
-                  }} className='cursor-pointer'>
-                      <h1 className={`text-xl px-7 ${selectionItemIndex === index ? `text-teal-200 border-teal-300 border-l-4 -ml-[3px] bg-[#4a5d87] py-3 sm:w-40` : `text-white`}`}>{capstone.title}</h1>
-                  </div>
-              ))}
-              </div>
-          <div className='flex flex-col gap-5'>
-          <h1 className='text-white text-2xl font-semibold'>{capstone[selectionItemIndex].title}</h1>
-          <h1 className='text-white text-xl'>{capstone[selectionItemIndex].lang}</h1>
-          <p className='text-white text-xl'>{capstone[selectionItemIndex].desc}</p>
-          
-          </div>
-          
-          </div>
-        
-      </div>
-    )
-  }
-  
+  const [selectionItemIndex, setSelectionItemIndex] = useState(0);
+  const [showPdf, setShowPdf] = useState(null);
 
-export default Capstone
+  const handleViewPdf = (pdfUrl) => {
+    setShowPdf(pdfUrl);
+  };
+
+  return (
+    <div>
+      <SectionTitle title={'Capstone Project 4.2'} />
+      <div className='flex py-10 gap-20 sm:flex-col'>
+        <div className='sm:flex-row sm:overflow-scroll sm:w-full flex flex-col gap-7 w-1/3 border-l-2 border-teal-200'>
+          {capstone.map((capstone, index) => (
+            <div
+              key={index}
+              onClick={() => setSelectionItemIndex(index)}
+              className='cursor-pointer'
+            >
+              <h1
+                className={`text-xl px-7 ${
+                  selectionItemIndex === index
+                    ? `text-teal-200 border-teal-300 border-l-4 -ml-[3px] bg-[#4a5d87] py-3 sm:w-40`
+                    : `text-white`
+                }`}
+              >
+                {capstone.title}
+              </h1>
+            </div>
+          ))}
+        </div>
+        <div className='flex flex-col gap-5'>
+          <h1 className='text-white text-2xl font-semibold'>
+            {capstone[selectionItemIndex].title}
+          </h1>
+          <h1 className='text-white text-xl'>
+            {capstone[selectionItemIndex].lang}
+          </h1>
+          <p className='text-white text-xl'>
+            {capstone[selectionItemIndex].desc}
+          </p>
+        </div>
+      </div>
+      <div className='flex gap-7 flex-wrap mt-12 items-center justify-center'>
+        <button
+          onClick={() => handleViewPdf(autoBio)}
+          className='text-white bg-blue-500 px-4 py-2 rounded'
+        >
+          View Autobiography (English)
+        </button>
+        <button
+          onClick={() => handleViewPdf(english)}
+          className='text-white bg-blue-500 px-4 py-2 rounded'
+        >
+          View CV (English)
+        </button>
+        <a
+          href={english}
+          download='Maxwell_GogoCV-English.pdf'
+          className='text-white bg-blue-500 px-4 py-2 rounded'
+        >
+          Download CV (English)
+        </a>
+        <a
+          href={swahili}
+          download='Maxwell_GogoCV-swahili.pdf'
+          className='text-white bg-blue-500 px-4 py-2 rounded'
+        >
+          Download CV (Kiswahili)
+        </a>
+        <a
+          href={luo}
+          download='Maxwell_GogoCV-LUO.pdf'
+          className='text-white bg-blue-500 px-4 py-2 rounded'
+        >
+          Download CV (Luo)
+        </a>
+      </div>
+      {showPdf && (
+        <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50'>
+          <div className='bg-white p-2 rounded-lg w-full h-full'>
+            <button
+              onClick={() => setShowPdf(null)}
+              className='float-right text-black'
+            >
+              Close
+            </button>
+            <iframe src={showPdf} title='PDF Viewer' width='100%' height='100%' />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Capstone;
